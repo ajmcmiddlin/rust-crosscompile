24f90e61dda3b1af080b971c340b208d0a79d682
Andrew McMiddlin <andrew@ajmccluskey.com>
2021-06-04 19:54:14 +1000

    Remove GTK from package and cleanup package dir.

--- package.sh
+++ package.sh
@@ -16,30 +16,7 @@ for DLL in $DLLS
     do cp "$DLL" package
 done
 
-mkdir -p package/share/{themes,gtk-3.0,glib-2.0}
-cp -r $GTK_INSTALL_PATH/share/glib-2.0/schemas package/share/glib-2.0/
-cp -r $GTK_INSTALL_PATH/share/icons package/share/icons
-
-if [ -z "$WIN_THEME" ]
-then
-	cat <<-EOF > package/share/gtk-3.0/settings.ini
-	[Settings]
-	gtk-font-name = Segoe UI 10
-	gtk-xft-rgba = rgb
-	gtk-xft-antialias = 1
-EOF
-else
-	cp -r /home/rust/Windows10 package/share/themes
-
-	cat <<-EOF > package/share/gtk-3.0/settings.ini
-	[Settings]
-	gtk-theme-name = Windows10
-	gtk-font-name = Segoe UI 10
-	gtk-xft-rgba = rgb
-	gtk-xft-antialias = 1
-	EOF
-fi
-
 find package -maxdepth 1 -type f -exec mingw-strip {} +
 
 zip -qr package.zip package/*
+rm -rf package
